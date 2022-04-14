38aa1603bec7034e48161e5b9a0a91ba934078bb
Mingyang Li <mingyangli118@gmail.com>
2022-04-14 13:52:43 +1200

    Fixed linting for types and modified namings

--- src/dto/Types.dto.ts
+++ src/dto/Types.dto.ts
@@ -1,173 +1,171 @@
-interface Calories {
-    dateTime: Date; // dateTime has a type of Date
-    value: number; // number type in typescript covers both int and floats
+export interface Calories {
+  dateTime: Date; // dateTime has a type of Date
+  value: number; // number type in typescript covers both int and floats
 }
 
-interface Distance {
-    dateTime: Date; // dateTime has a type of Date
-    value: number; // number type in typescript covers both int and floats
-  }
-
-  interface ActivityLevel {
-    minutes: number;
-    name: string;
+export interface Distance {
+  dateTime: Date; // dateTime has a type of Date
+  value: number; // number type in typescript covers both int and floats
 }
 
-interface ManualValuesSpecified {
-    calories: boolean;
-    distance: boolean;
-    steps: boolean;
+export interface ActivityLevel {
+  minutes: number;
+  name: string;
 }
 
-
-interface HeartRateZone {
-    name: string;
-    min: number;
-    max: number;
-    minutes: number;
+export interface ManualValuesSpecified {
+  calories: boolean;
+  distance: boolean;
+  steps: boolean;
 }
 
-interface Exercise {
-    activityLevel: ActivityLevel;
-    manualValuesSpecified: ManualValuesSpecified;
-    heartRateZone: HeartRateZone;
-
-    logId: number;
-    activityName: string;
-    activityTypeId: number;
-    averageHeartRate: number;
-    calories: number;
-    duration: number;
-    activeDuration: number;
-    steps: number;
-    logType: string;
-    heartRateZones: HeartRateZone[];
-    lastModified: string;
-    startTime: string;
-    originalStartTime: string;
-    originalDuration: number;
-    elevationGain: number;
-    hasGps: boolean;
-    shouldFetchDetails: boolean;
-};
-
-interface Value {
-    bpm: number;
-    confidence: number;
+export interface HeartRateZone {
+  name: string;
+  min: number;
+  max: number;
+  minutes: number;
 }
 
-interface Heart_rate {
-    value: Value;
+export interface Exercise {
+  activityLevel: ActivityLevel;
+  manualValuesSpecified: ManualValuesSpecified;
+  heartRateZone: HeartRateZone;
+
+  logId: number;
+  activityName: string;
+  activityTypeId: number;
+  averageHeartRate: number;
+  calories: number;
+  duration: number;
+  activeDuration: number;
+  steps: number;
+  logType: string;
+  heartRateZones: HeartRateZone[];
+  lastModified: string;
+  startTime: string;
+  originalStartTime: string;
+  originalDuration: number;
+  elevationGain: number;
+  hasGps: boolean;
+  shouldFetchDetails: boolean;
 }
 
-interface RootObject {
-    dateTime: Date;
-    value: Value;
+export interface Value {
+  bpm: number;
+  confidence: number;
 }
 
-interface Lightly_active_minutes {
-    dateTime: Date;
+export interface HeartRate {
+  value: Value;
+}
+
+export interface RootObject {
+  dateTime: Date;
+  value: Value;
+}
+
+export interface LightlyActiveMinutes {
+  dateTime: Date;
   value: number;
 }
 
-interface Moderatly_active_minutes {
-    dateTime: Date;
+export interface ModeratlyActiveMinutes {
+  dateTime: Date;
   value: number;
 }
 
-interface Moderatly_active_minutes {
-    dateTime: Date;
+export interface ModeratlyActiveMinutes {
+  dateTime: Date;
   value: number;
 }
 
-interface Resting_heart_rate {
-    dateTime: Date;
-    value: Value;
+export interface RestingHeartRate {
+  dateTime: Date;
+  value: Value;
 }
 
-interface Sedentary_minutes {
-    dateTime: Date;
-    value: number;
-}
-
-
-interface Deep {
-    count: number;
-    minutes: number;
-    thirtyDayAvgMinutes: number;
-}
-
-interface Wake {
-    count: number;
-    minutes: number;
-    thirtyDayAvgMinutes: number;
-}
-
-interface Light {
-    count: number;
-    minutes: number;
-    thirtyDayAvgMinutes: number;
-}
-
-interface Rem {
-    count: number;
-    minutes: number;
-    thirtyDayAvgMinutes: number;
-}
-
-interface Summary {
-    deep: Deep;
-    wake: Wake;
-    light: Light;
-    rem: Rem;
-}
-
-interface Datum {
-    dateTime: Date;
-    level: string;
-    seconds: number;
-}
-
-interface ShortData {
-    dateTime: Date;
-    level: string;
-    seconds: number;
-}
-
-interface Levels {
-    summary: Summary;
-    data: Datum[];
-    shortData: ShortData[];
-}
-interface Sleep {
-    logId: number;
-    dateOfSleep: Date;
-    startTime: Date;
-    endTime: Date;
-    duration: number;
-    minutesToFallAsleep: number;
-    minutesAsleep: number;
-    minutesAwake: number;
-    minutesAfterWakeup: number;
-    timeInBed: number;
-    efficiency: number;
-    type: string;
-    infoCode: number;
-    levels: Levels;
-    mainSleep: boolean;
-}
-
- interface Steps{
-	dateTime: Date;
+export interface SedentaryMinutes {
+  dateTime: Date;
   value: number;
 }
 
-interface Steps{
-	dateTime: Date;
+export interface Deep {
+  count: number;
+  minutes: number;
+  thirtyDayAvgMinutes: number;
+}
+
+export interface Wake {
+  count: number;
+  minutes: number;
+  thirtyDayAvgMinutes: number;
+}
+
+export interface Light {
+  count: number;
+  minutes: number;
+  thirtyDayAvgMinutes: number;
+}
+
+export interface Rem {
+  count: number;
+  minutes: number;
+  thirtyDayAvgMinutes: number;
+}
+
+export interface Summary {
+  deep: Deep;
+  wake: Wake;
+  light: Light;
+  rem: Rem;
+}
+
+export interface Datum {
+  dateTime: Date;
+  level: string;
+  seconds: number;
+}
+
+export interface ShortData {
+  dateTime: Date;
+  level: string;
+  seconds: number;
+}
+
+export interface Levels {
+  summary: Summary;
+  data: Datum[];
+  shortData: ShortData[];
+}
+export interface Sleep {
+  logId: number;
+  dateOfSleep: Date;
+  startTime: Date;
+  endTime: Date;
+  duration: number;
+  minutesToFallAsleep: number;
+  minutesAsleep: number;
+  minutesAwake: number;
+  minutesAfterWakeup: number;
+  timeInBed: number;
+  efficiency: number;
+  type: string;
+  infoCode: number;
+  levels: Levels;
+  mainSleep: boolean;
+}
+
+export interface Steps {
+  dateTime: Date;
   value: number;
 }
 
-interface Very_active_minutes {
-	dateTime: Date;
+export interface Steps {
+  dateTime: Date;
+  value: number;
+}
+
+export interface VeryActiveMinutes {
+  dateTime: Date;
   value: number;
 }
