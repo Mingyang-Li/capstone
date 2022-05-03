973ea3958a1a21b02655c050e0f148be2eaf5e64
Mingyang Li <mingyangli118@gmail.com>
2022-05-03 22:01:02 +1200

    Init view DISTANCE_BY_DATE

--- .hasura/views.sql
+++ .hasura/views.sql
@@ -2,3 +2,8 @@ CREATE OR REPLACE VIEW "STEPS_BY_DATE" AS
   SELECT date, sum(value), "public"."Steps"."userId"
     FROM "public"."Steps"
     GROUP BY date, "public"."Steps"."userId"
+
+CREATE OR REPLACE VIEW "DISTANCE_BY_DATE" AS 
+  SELECT date, sum(value), "public"."Distance"."userId"
+    FROM "public"."Distance"
+    GROUP BY date, "public"."Distance"."userId"
