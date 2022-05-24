cf15bb43a60b8140d799b1ab43b87576c12dbe01
Mingyang Li <mingyangli118@gmail.com>
2022-05-24 15:04:24 +1200

    Init steps creation service

---
+++ db/src/dto/steps.ts
@@ -0,0 +1,59 @@
+import { User } from './user';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+export class Steps {
+  @ApiProperty({ type: Number })
+  id: number;
+
+  @ApiPropertyOptional({ type: Date })
+  dateTime?: Date;
+
+  @ApiPropertyOptional({ type: Date })
+  date?: Date;
+
+  @ApiPropertyOptional({ type: Number })
+  value?: number;
+
+  @ApiPropertyOptional({ type: Number })
+  userId?: number;
+
+  @ApiPropertyOptional({ type: () => User })
+  User?: User;
+}
+
+export class CreateStepsArgs {
+  @ApiPropertyOptional({ type: Number })
+  id?: number;
+
+  @ApiPropertyOptional({ type: Date })
+  dateTime?: Date;
+
+  @ApiPropertyOptional({ type: Date })
+  date?: Date;
+
+  @ApiPropertyOptional({ type: Number })
+  value?: number;
+
+  @ApiPropertyOptional({ type: Number })
+  userId?: number;
+}
+
+export class CreateStepsResponse {
+  @ApiPropertyOptional({ type: Number })
+  id: number;
+
+  @ApiPropertyOptional({ type: Date })
+  dateTime?: Date;
+
+  @ApiPropertyOptional({ type: Date })
+  date?: Date;
+
+  @ApiPropertyOptional({ type: Number })
+  value?: number;
+
+  @ApiPropertyOptional({ type: Number })
+  userId?: number;
+
+  @ApiPropertyOptional({ type: () => User })
+  User?: User;
+}
--- db/src/services/Step.service.ts
+++ db/src/services/Step.service.ts
@@ -1,6 +1,21 @@
+import { CreateStepsArgs } from './../dto/steps';
 import { PrismaService } from './Prisma.service';
 import { Inject } from '@nestjs/common';
+import { Prisma, Steps } from '@prisma/client';
 
 export class StepsService {
   constructor(@Inject() private prismaService: PrismaService) {}
+
+  async createSteps(
+    param: CreateStepsArgs,
+  ): Promise<Prisma.Prisma__StepsClient<Steps>> {
+    return this.prismaService.steps.create({
+      data: {
+        dateTime: param.dateTime,
+        value: param.value,
+        date: param.date,
+        userId: param.userId,
+      },
+    });
+  }
 }
