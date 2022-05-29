8e7d4aec109abaa154ccc1566a795be2d6ae2f9b
Mingyang Li <mingyangli118@gmail.com>
2022-05-30 11:35:19 +1200

    Created views for new tables

--- .hasura/views.sql
+++ .hasura/views.sql
@@ -7,3 +7,13 @@ CREATE OR REPLACE VIEW "DISTANCE_BY_DATE" AS
   SELECT date, sum(value), "public"."Distance"."userId"
     FROM "public"."Distance"
     GROUP BY date, "public"."Distance"."userId"
+
+CREATE OR REPLACE VIEW "CALORIES_BY_DATE" AS 
+  SELECT date, sum(value), "public"."Calories"."userId"
+    FROM "public"."Calories"
+    GROUP BY date, "public"."Calories"."userId"
+
+CREATE OR REPLACE VIEW "VERY_ACTIVE_MINUTES_BY_DATE" AS 
+  SELECT date, sum(value), "public"."VeryActiveMinutes"."userId"
+    FROM "public"."VeryActiveMinutes"
+    GROUP BY date, "public"."VeryActiveMinutes"."userId"
