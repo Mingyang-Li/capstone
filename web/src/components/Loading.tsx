6f3d34474130cad7210f6e2f6383d2193e08e56a
Mingyang Li <mingyangli118@gmail.com>
2022-05-31 17:28:16 +1200

    Setup auth flow with protected routes

--- web/src/components/Loading.tsx
+++ web/src/components/Loading.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+
 const loadingImg =
   "https://cdn.auth0.com/blog/auth0-react-sample/assets/loading.svg";
 
 const Loading = () => (
-  <div className="spinner">
+  <div
+    className="spinner"
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      verticalAlign: "baseline",
+    }}
+  >
     <img src={loadingImg} alt="Loading..." />
   </div>
 );
--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -1,9 +1,20 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect } from "react";
 import LogoutButton from "../components/auth/LogoutButton";
+import Loading from "../components/Loading";
+import { Forbidden } from "./Forbidden";
 
 export const Dashboard: React.FC = () => {
+  const { isAuthenticated, isLoading } = useAuth0();
+  useEffect(() => {
+    console.log("isAuthenticated: " + isAuthenticated);
+  }, [isAuthenticated]);
+  if (isLoading) return <Loading />;
+  if (!isAuthenticated) return <Forbidden />;
   return (
     <>
       <h2>DashboardPage</h2>
+      <h3>isAuthenticated: {isAuthenticated}</h3>
       <LogoutButton />
     </>
   );
